7e6c66226605c6b28cc6862940955ab1321d55bd
Jemesson <jemessonlima@gmail.com>
2020-08-05 18:16:49 -0300

    Add reactjs component to add & remove repositories and pass tests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(getRepositories, []);
+
   async function handleAddRepository() {
-    // TODO
+    const payload = {
+      url: `https://github.com/Jemesson`,
+      title: `ReactJS bootcamp challenge_${Date.now()}`,
+      techs: ["React", "Node.js"],
+    }
+    const response = await api.post('repositories', payload);
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    const filteredRepositories = repositories.filter(repository => repository.id !== id);
+    setRepositories(filteredRepositories);
+  }
+
+  function getRepositories() {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map(repository =>
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          )
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
